0eb48a2e6c46b73861409dc24267797081a99b09
professornaite <professornaite@gmail.com>
2024-11-13 20:21:56 -0500

    web

---
+++ code/add-shiny-to-web.R
@@ -0,0 +1,11 @@
+# add shiny to web
+
+# Goal is to host the CritBot shiny app on the github page for CritBot
+
+# packages
+install.packages("shiny")
+install.packages("shinylive")
+
+# convert shiny to shinylive
+library(shinylive)
+shinylive::export(appdir = "critbot-shiny", destdir = "docs")
